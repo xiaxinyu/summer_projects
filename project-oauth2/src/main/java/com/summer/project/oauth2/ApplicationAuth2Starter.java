f882d1141becbd58880b7a6fa11e676ac3ef15e5
summer.xia <summer_west2010@126.com>
2019-04-05 16:01:24 +0800

    添加注释

--- project-oauth2/src/main/java/com/summer/project/oauth2/ApplicationAuth2Starter.java
+++ project-oauth2/src/main/java/com/summer/project/oauth2/ApplicationAuth2Starter.java
@@ -3,6 +3,9 @@ package com.summer.project.oauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by summer.xia on 2019.4.5
+ */
 @SpringBootApplication
 public class ApplicationAuth2Starter {
 	public static void main(String[] args) {
