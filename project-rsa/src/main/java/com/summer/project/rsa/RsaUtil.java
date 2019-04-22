d9513cfcdf28529a1ad2ebf8a58a0e15a98e0321
xiaxinyu3 <xiaxinyu3@crc.com.hk>
2019-04-22 17:04:38 +0800

    create rsa project

---
+++ project-rsa/src/main/java/com/summer/project/rsa/RsaUtil.java
@@ -0,0 +1,84 @@
+package com.summer.project.rsa;
+
+import javax.crypto.Cipher;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author summer_west2010@126.com
+ */
+public class RsaUtil {
+    /**
+     * 随机生成密钥对
+     *
+     * @throws NoSuchAlgorithmException
+     */
+    public static Map<String, String> genKeyPair() throws NoSuchAlgorithmException {
+        Map<String, String> keys = new HashMap<>(2);
+
+        // KeyPairGenerator类用于生成公钥和私钥对，基于RSA算法生成对象
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+        // 初始化密钥对生成器，密钥大小为96-1024位
+        keyPairGen.initialize(1024, new SecureRandom());
+        // 生成一个密钥对，保存在keyPair中
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        // 得到私钥
+        PrivateKey privateKey = keyPair.getPrivate();
+        // 得到公钥
+        PublicKey publicKey = keyPair.getPublic();
+        String publicKeyString = new String(Base64.getEncoder().encode(publicKey.getEncoded()));
+        // 得到私钥字符串
+        String privateKeyString = new String(Base64.getEncoder().encode((privateKey.getEncoded())));
+        // 将公钥和私钥保存到Map
+        keys.put("public", publicKeyString);
+        keys.put("private", privateKeyString);
+
+        return keys;
+    }
+
+    /**
+     * 加密字符串
+     *
+     * @param str       待加密字符串
+     * @param publicKey 公钥
+     * @return 加密字符串
+     * @throws Exception
+     */
+    public static String encrypt(String str, String publicKey) throws Exception {
+        //base64编码的公钥
+        byte[] decoded = Base64.getDecoder().decode(publicKey);
+        RSAPublicKey pubKey = (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(decoded));
+        //RSA加密
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+        String outStr = new String(Base64.getEncoder().encode(cipher.doFinal(str.getBytes("UTF-8"))));
+        return outStr;
+    }
+
+    /**
+     * 解密字符串
+     *
+     * @param str        待解密字符串
+     * @param privateKey 私钥
+     * @return 解密后字符串
+     * @throws Exception
+     */
+    public static String decrypt(String str, String privateKey) throws Exception {
+        //64位解码加密后的字符串
+        byte[] inputByte = Base64.getDecoder().decode(str.getBytes("UTF-8"));
+        //base64编码的私钥
+        byte[] decoded = Base64.getDecoder().decode(privateKey);
+        RSAPrivateKey priKey = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(decoded));
+        //RSA解密
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, priKey);
+        String outStr = new String(cipher.doFinal(inputByte));
+        return outStr;
+    }
+}
---
+++ project-rsa/src/test/java/com/summer/project/rsa/RsaUtilTest.java
@@ -0,0 +1,29 @@
+package com.summer.project.rsa;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class RsaUtilTest {
+
+    @Test
+    public void testRas(){
+        try{
+            Map<String, String> keys = RsaUtil.genKeyPair();
+            String publicKey = keys.get("public");
+            String privateKey = keys.get("private");
+            System.out.println(publicKey);
+            System.out.println();
+            System.out.println(privateKey);
+
+            String testStr = "abc@123";
+            String encryptedStr = RsaUtil.encrypt(testStr, publicKey);
+            String decryptedStr = RsaUtil.decrypt(encryptedStr, privateKey);
+
+            Assert.assertTrue(testStr.equals(decryptedStr));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
